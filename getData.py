8f754b339bba77b374c42bcacf9c3c9f26214e3b
Mitesh Chudasama <mitesh.chudasama@j-kri.com>
2016-12-15 18:46:59 +0530

    Added try catch

--- getData.py
+++ getData.py
@@ -9,22 +9,21 @@ conn = redis.Redis('localhost')
 def scrapData():
 	count = 0
 	while True:
+		try:    		
+			req = requests.get("https://www.nseindia.com/live_market/dynaContent/live_analysis/gainers/niftyGainers1.json")
+			data = req.json()
+			
+			count += 1
 
-		req = requests.get("https://www.nseindia.com/live_market/dynaContent/live_analysis/gainers/niftyGainers1.json")
-		data = req.json()
+			setData = data
+			setData["lasupdatedtime"] = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+			setData["count"] = count
+			nifty50Data = json.dumps(setData)
+			conn.set("nifty50Data", nifty50Data)
+			conn.set("lasupdatedtime", setData["lasupdatedtime"])
 
-		count += 1
-
-		setData = data
-		setData["lasupdatedtime"] = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-		setData["count"] = count
-		nifty50Data = json.dumps(setData)
-		conn.set("nifty50Data", nifty50Data)
-		conn.set("lasupdatedtime", setData["lasupdatedtime"])
-
+		except requests.exceptions.ConnectionError:
+    		 print "Connection refused"
 
 		time.sleep(300)
 
-
-
-	# print conn.hgetall(str(each.get("symbol")))

bba2f91abe1b6f92a1f6978a45153459900564a1
Mitesh Chudasama <mitesh.chudasama@j-kri.com>
2016-12-15 21:55:48 +0530

    Testing on heroku

--- app.py
+++ app.py
@@ -20,11 +20,13 @@ if __name__ == '__main__':
             'tools.staticdir.dir': './public'
         }
     }
-    # background_thread = Thread(target=scrapData)
-    # background_thread.start()
 
     cherrypy.config.update({'server.socket_host': '0.0.0.0'})
     cherrypy.config.update({'server.socket_port': int(os.environ.get("PORT", 5000))})
+
+
+    background_thread = Thread(target=scrapData)
+    background_thread.start()
 
 
     webapp = Nifty50Diplay()

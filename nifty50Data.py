0173946b53c46fce7a6e01df6b0ffaa014bce2cf
Mitesh Chudasama <mitesh.chudasama@j-kri.com>
2016-12-15 22:04:36 +0530

    Testing on heroku

--- nifty50Data.py
+++ nifty50Data.py
@@ -14,6 +14,6 @@ class Nifty50WebService(object):
 
     # @cherrypy.tools.accept(media='text/json')
     def GET(self):
-        conn = redis.Redis('localhost')
+        conn = redis.Redis(host="pub-redis-13650.us-east-1-2.1.ec2.garantiadata.com", port=13650, password="redis1234")
         data = conn.get("nifty50Data")        
         return data

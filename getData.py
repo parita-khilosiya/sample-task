5ed510b0b648f4a5ba89a5b108871059ce403c57
Parita Khilosiya <pari.khilosiya@gmail.com>
2017-03-04 20:56:28 +0530

    updated redis url

--- getData.py
+++ getData.py
@@ -4,7 +4,8 @@ import datetime
 import time
 
 import redis
-conn = redis.Redis(host="pub-redis-13650.us-east-1-2.1.ec2.garantiadata.com", port=13650, password="redis1234")
+conn = redis.Redis(host="redis-13633.c10.us-east-1-2.ec2.cloud.redislabs.com", port=13633, password="redis1234")     
+# conn = redis.Redis(host="pub-redis-13650.us-east-1-2.1.ec2.garantiadata.com", port=13650, password="redis1234")
 
 def scrapData():
 	# count = 0
--- nifty50Data.py
+++ nifty50Data.py
@@ -14,6 +14,7 @@ class Nifty50WebService(object):
 
     # @cherrypy.tools.accept(media='text/json')
     def GET(self):
-        conn = redis.Redis(host="pub-redis-13650.us-east-1-2.1.ec2.garantiadata.com", port=13650, password="redis1234")
+        # conn = redis.Redis(host="pub-redis-13650.us-east-1-2.1.ec2.garantiadata.com", port=13650, password="redis1234")
+        conn = redis.Redis(host="redis-13633.c10.us-east-1-2.ec2.cloud.redislabs.com", port=13633, password="redis1234")   
         data = conn.get("nifty50Data")        
         return data
